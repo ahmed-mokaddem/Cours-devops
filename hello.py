88560e9370f4381239f38337a0f3e01fbbd86eba
Ahmed Mokaddem <ahm.moka@gmail.com>
2022-10-22 18:54:49 +0200

    first version

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+  
+@app.route('/')
+def hello():
+    return "Welcome to my the first Project"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5002)
